740ac460a164524e52503b3f62a2248b8f2327af
VictorGamer072YT <44535997+VictorGamer072YT@users.noreply.github.com>
2022-09-21 23:04:59 -0300

    Update essentialFiles.js

--- essentialFiles.js
+++ essentialFiles.js
@@ -12,6 +12,8 @@ module.exports = [
     'EOSSDK-Win64-Shipping.dll',
     // RDR 2 Cleaner
     'rdr2cleaner.exe',
+    'app.js',
+    'essentialFiles.js',
     'modstore',
     // Main Game Files
     '12on7',
